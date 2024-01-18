b2f9c7091238c25c00f1daf65388f54cece01028
Lucy Davinhart <lucy@lucymhdavies.com>
2024-01-18 11:31:32 +0000

    TF-11772: resource_group_location output should actually be an output

--- rg/outputs.tf
+++ rg/outputs.tf
@@ -2,6 +2,6 @@ output "resource_group_name" {
   value = azurerm_resource_group.example.name
 }
 
-variable "resource_group_location" {
+output "resource_group_location" {
   value = azurerm_resource_group.example.location
 }
